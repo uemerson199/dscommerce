e891e05b0b50280f261964e18b41437b4d814cde
Uemerson Ribeiro <uemersonribeiro220@gmail.com>
2024-08-08 18:21:34 -0300

    POST new product

--- dscommerce/src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -9,9 +9,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.Optional;
-
 @Service
 public class ProductService {
 
@@ -30,5 +27,19 @@ public class ProductService {
         Page<Product> result = repository.findAll(pageable);
         return result.map(x -> new ProductDTO(x));
     }
+
+    @Transactional
+    public ProductDTO insert(ProductDTO dto) {
+      Product entity = new Product();
+      entity.setName(dto.getName());
+      entity.setDescription(dto.getDescription());
+      entity.setPrice(dto.getPrice());
+      entity.setImgUrl(dto.getImgUrl());
+
+      entity = repository.save(entity);
+
+      return new ProductDTO(entity);
+    }
+
 }
 
