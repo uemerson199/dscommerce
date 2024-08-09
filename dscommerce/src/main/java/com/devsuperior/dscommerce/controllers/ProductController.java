6bc87bc864c5a8254658fef7cd384f6e04aa5aee
Uemerson Ribeiro <uemersonribeiro220@gmail.com>
2024-08-09 12:02:04 -0300

    DELETE product

--- dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -42,6 +42,13 @@ public class ProductController {
         return ResponseEntity.ok(dto);
     }
 
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
 
 
 
