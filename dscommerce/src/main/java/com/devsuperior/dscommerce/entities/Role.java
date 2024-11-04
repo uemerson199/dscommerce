ebde4f789a5e9175ca634d36b93fd425440a2caa
Uemerson Ribeiro <uemersonribeiro220@gmail.com>
2024-11-04 12:57:45 -0300

    UserDetails, GrantedAuthority

--- dscommerce/src/main/java/com/devsuperior/dscommerce/entities/Role.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/entities/Role.java
@@ -1,13 +1,14 @@
 package com.devsuperior.dscommerce.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Objects;
 
 
 @Entity
 @Table(name = "tb_role")
-public class Role {
+public class Role implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,6 +32,7 @@ public class Role {
         this.id = id;
     }
 
+    @Override
     public String getAuthority() {
         return authority;
     }
