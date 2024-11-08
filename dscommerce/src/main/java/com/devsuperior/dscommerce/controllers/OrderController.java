e28303db280bcfb9629c44a9b64f3e7c0c780811
Uemerson Ribeiro <uemersonribeiro220@gmail.com>
2024-11-08 10:36:22 -0300

    Saving Order

--- dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/OrderController.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/OrderController.java
@@ -2,13 +2,9 @@ package com.devsuperior.dscommerce.controllers;
 
 import com.devsuperior.dscommerce.dto.OrderDTO;
 import com.devsuperior.dscommerce.dto.ProductDTO;
-import com.devsuperior.dscommerce.dto.ProductMinDTO;
 import com.devsuperior.dscommerce.services.OrderService;
-import com.devsuperior.dscommerce.services.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -28,6 +24,14 @@ public class OrderController {
     public ResponseEntity<OrderDTO> findById(@PathVariable Long id) {
         OrderDTO dto = service.findById(id);
         return ResponseEntity.ok(dto);
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_CLIENT')")
+    @PostMapping
+    public ResponseEntity<OrderDTO> insert(@Valid @RequestBody OrderDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 
 
--- dscommerce/src/main/java/com/devsuperior/dscommerce/services/OrderService.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/services/OrderService.java
@@ -1,9 +1,9 @@
 package com.devsuperior.dscommerce.services;
 
 import com.devsuperior.dscommerce.dto.OrderDTO;
-import com.devsuperior.dscommerce.dto.ProductDTO;
-import com.devsuperior.dscommerce.entities.Order;
-import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.dto.OrderItemDTO;
+import com.devsuperior.dscommerce.entities.*;
+import com.devsuperior.dscommerce.repositories.OrderItemRepository;
 import com.devsuperior.dscommerce.repositories.OrderRepository;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
 import com.devsuperior.dscommerce.services.exceptions.ResourceNotFoundException;
@@ -11,12 +11,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
+
 @Service
 public class OrderService {
 
 
     @Autowired
     private OrderRepository repository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
+
+    @Autowired
+    UserService userService;
 
     @Transactional(readOnly = true)
     public OrderDTO findById(Long id) {
@@ -26,5 +37,26 @@ public class OrderService {
     }
 
 
+    @Transactional
+    public  OrderDTO insert(OrderDTO dto) {
 
+        Order order = new Order();
+        order.setMoment(Instant.now());
+        order.setStatus(OrderStatus.WAITING_PAYMENT);
+
+        User user = userService.authenticated();
+        order.setClient(user);
+
+        for (OrderItemDTO itemDTO : dto.getItems()) {
+            Product product = productRepository.getReferenceById(itemDTO.getProductId());
+            OrderItem item = new OrderItem(order, product, itemDTO.getQuantity(), itemDTO.getPrice());
+            order.getItems().add(item);
+
+        }
+
+        repository.save(order);
+        orderItemRepository.saveAll(order.getItems());
+        return new OrderDTO(order);
+
+    }
 }
